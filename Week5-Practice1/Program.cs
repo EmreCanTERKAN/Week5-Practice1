2d1fb5bdf11174c61535aefc14b2d130f4dd31f8
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-08-15 10:29:53 +0300

    Add project files.

---
+++ Week5-Practice1/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] tamsayilar = new int[10];
+
+for (int i = 0; i < tamsayilar.Length; i++)
+{
+    Console.WriteLine($"Lütfen {i+1}. sayıyı giriniz:");
+    tamsayilar[i] = int.Parse(Console.ReadLine());
+}
+
+foreach (int i in tamsayilar)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("Diziye eklemek istediğin 11. elemanı giriniz");
+int yeniSayi = int.Parse(Console.ReadLine());
+
+int[] yeniDizi = new int[tamsayilar.Length + 1];
+
+for (int i = 0; i< tamsayilar.Length; i++)
+{
+    yeniDizi[i] = tamsayilar[i];
+}
+
+yeniDizi[yeniDizi.Length - 1] = yeniSayi;
+
+Console.WriteLine("_________________________________");
+
+var shortednumber = yeniDizi.OrderByDescending(n => n);
+
+foreach (int number in shortednumber)
+{
+    Console.WriteLine(number);
+}
